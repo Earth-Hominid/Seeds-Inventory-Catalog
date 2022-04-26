386ae8ba3a6cbaafd6d39ed9e8191e56b7590431
C <earthhominid@gmail.com>
2022-04-26 10:31:35 -0300

    server running ok, mongodb connected ok

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+var path = require('path');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const { handleError } = require('./middleware/errorMiddleware');
 const connectDatabase = require('./config/database');
 const port = process.env.PORT || 5001;
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 connectDatabase();
 const app = express();
 
-app.set('views', __dirname);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// error handler
+app.use(handleError);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
