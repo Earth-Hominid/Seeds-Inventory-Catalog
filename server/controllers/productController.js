93f2873df30f8bf1193b6e8f9dbfe64593a3f64c
C <earthhominid@gmail.com>
2022-05-06 13:39:04 -0300

    logic added to both GET and POST update product methods

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -234,9 +234,122 @@ exports.product_delete_post = (req, res, next) => {
 };
 
 // Display product update form on GET request.
-exports.product_update_get = (req, res) =>
-  res.send('Not implemented: product update GET');
+exports.product_update_get = (req, res, next) => {
+  // Get product, categories, sub-categories, and department for form.
+
+  async.parallel(
+    {
+      product: (callback) => {
+        Product.findById(req.params.id)
+          .populate('category subcategory department')
+          .exec(callback);
+      },
+      categories: (callback) => {
+        Category.find(callback);
+      },
+      subcategories: (callback) => {
+        Subcategory.find(callback);
+      },
+      departments: (callback) => {
+        Department.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) () => next(err);
+      if (results.product == null) {
+        // No results.
+        let err = new Error('Product not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      res.render('product_form', {
+        title: 'Update Product',
+        categories: results.categories,
+        subcategories: results.subcategories,
+        departments: results.departments,
+        product: results.product,
+      });
+    }
+  );
+};
 
 // Handle product update on POST.
-exports.product_update_post = (req, res) =>
-  res.send('Not implemented: product update POST');
+exports.product_update_post = [
+  // Validate and sanitize fields.
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Name needs to be specified.'),
+  body('stockNumber')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Stock number needs to be specified.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Description needs to be provided.'),
+  body('packageSize')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('A size needs to be provided.'),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Product object with escaped and trimmed data.
+    const product = new Product({
+      name: req.body.name,
+      department: req.body.department,
+      category: req.body.category,
+      subcategory: req.body.subcategory,
+      description: req.body.description,
+      stockNumber: req.body.stockNumber,
+      price: req.body.price,
+      packageSize: req.body.packageSize,
+      _id: req.params.id, // This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel({
+        categories: (callback) => Category.find(callback),
+        subcategories: (callback) => Subcategory.find(callback),
+        departments: (callback) => Department.find(callback),
+      }),
+        (err, results) => {
+          if (err) () => next(err);
+          res.render('product_form', {
+            title: 'Update Product',
+            categories: results.categories,
+            subcategories: results.subcategories,
+            departments: results.departments,
+            product: product,
+            errors: errors.array(),
+          });
+        };
+      return;
+    } else {
+      // Data form is valid. Update the record.
+      Product.findByIdAndUpdate(
+        req.params.id,
+        product,
+        {},
+        function (err, theProduct) {
+          if (err)
+            () => next(err),
+              // Successful - redirect to book detail page.
+              res.redirect(theProduct.url);
+        }
+      );
+    }
+  },
+];
