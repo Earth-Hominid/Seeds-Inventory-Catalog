fc5cb3c0ad8fbd721f21c0b35ee9a1c7f3df3821
C <earthhominid@gmail.com>
2022-05-07 18:00:25 -0300

    changed multer middleware

--- server/middleware/multerMiddleware.js
+++ server/middleware/multerMiddleware.js
@@ -3,14 +3,16 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, '../public/images');
+    callback(null, '../public/data/uploads/');
   },
   filename: (req, file, callback) => {
     console.log(file);
-    callback(null, Date.now() + path.extname(file.originalname));
+    callback(null, file.fieldname + '-' + Date.now());
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+});
 
 module.exports = { upload };
