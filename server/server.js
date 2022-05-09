1bf5b8634e3b2fb6e844d3b7b77bb4e8c481ce31
C <earthhominid@gmail.com>
2022-05-09 15:59:08 -0300

    updated app.listen

--- server/server.js
+++ server/server.js
@@ -32,4 +32,4 @@ app.use('/catalog', catalogRouter);
 // error handler
 app.use(handleError);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(process.env.PORT, '0.0.0.0');
